136641fec8f68a5ec42f2b2a86377353ba1b774a
Seroga Humenchuk <niva1999niva@gmail.com>
2019-03-02 15:44:05 +0200

    add lesson for M i Y

---
+++ Y and M individual lesson/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//     let a = +prompt('Enter first number');
+//     let b = +prompt('Enter second number');
+
+//     console.log('first number = ' + a);
+//     console.log('second number = ' + b);
+
+//     console.log('num1 + num2 = ', a + b);
+//     console.log('num1 - num2 = ', a - b);
+//     console.log('num1 / num2 = ', a / b);
+//     console.log('num1 * num2 = ', a * b);
+
+//     console.log('num1 > num2 = ', a > b);
+//     console.log('num1 < num2 = ', a < b);
+
+// if(a > b) {
+//     console.log('a > b');
+// } else if (a === b) {
+//     console.log('a = b');
+// } else if (a < b){
+//     console.log('a < b');
+// }
+
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+
+// let pass = 'qwerty321';
+// let userPass;
+
+// do {
+//     userPass = prompt('Enter pass from WiFi')
+//     if (userPass === pass) {
+//         alert('Connected')
+//     } else {
+//         alert('Wrong pass, Enter correct pass')
+//     }
+// } while(userPass !== pass)
+
+
+
+
+
+
+
+
+///////////////////////////////////
+
+// const name = 'Mango';
+// const age = 2;
+// const mood = 'happy';
+
+// console.log("my name is " + name + ', I am ' + age + ', I am ' + mood);
+// console.log(`my name is ${name}, I am ${age}, I am ${mood}`);
